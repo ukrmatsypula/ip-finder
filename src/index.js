9152fc8b3c838f05d5471f839d1585a6391a2357
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-14 23:05:13 +0300

    render info in info blocks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { validateIp } from "./helpers";
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
 
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispIndo = document.querySelector("#isp");
+
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
@@ -12,7 +17,7 @@ function getData() {
       `https://geo.ipify.org/api/v2/country?apiKey=at_EJPw8rIExX3ENgGZLIOzxNwAJbkhu&ipAddress=${ipInput.value}`
     )
       .then((response) => response.json())
-      .then(console.log);
+      .then((data) => setInfo(data));
   }
 }
 
@@ -20,4 +25,11 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  ipInfo.textContent = mapData.ip;
+  locationInfo.textContent = `${mapData.location.country} ${mapData.location.region}`;
+  timezoneInfo.textContent = mapData.location.timezone;
+  ispIndo.textContent = mapData.isp;
 }
