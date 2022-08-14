5c1ee18b430e2302fc5f6c6a33da82f615ba6f1e
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-14 22:52:46 +0300

    add validate IP address

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIp } from "./helpers";
+
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
 
@@ -5,11 +7,13 @@ btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
 function getData() {
-  fetch(
-    `https://geo.ipify.org/api/v2/country?apiKey=at_EJPw8rIExX3ENgGZLIOzxNwAJbkhu&ipAddress=${ipInput.value}`
-  )
-    .then((response) => response.json())
-    .then(console.log);
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_EJPw8rIExX3ENgGZLIOzxNwAJbkhu&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
