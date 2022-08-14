6b792cc7bd6ac76966d351bc3e7b7b6ea44aa65e
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-14 22:43:10 +0300

    get ip address location in console

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector(".search-bar__btn");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_EJPw8rIExX3ENgGZLIOzxNwAJbkhu&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
