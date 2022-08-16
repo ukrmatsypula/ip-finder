d75126fdbc4e8810378c14e9af6303dd57166e3b
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-16 19:30:20 +0300

    add leaflet map and render map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { validateIp } from "./helpers";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+import { addTileLayer, validateIp } from "./helpers";
+import icon from "../images/icon-location.svg";
 
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
@@ -7,6 +11,22 @@ const ipInfo = document.querySelector("#ip");
 const locationInfo = document.querySelector("#location");
 const timezoneInfo = document.querySelector("#timezone");
 const ispIndo = document.querySelector("#isp");
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+  // iconAnchor: [22, 94],
+});
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+});
+
+addTileLayer(map);
+
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
